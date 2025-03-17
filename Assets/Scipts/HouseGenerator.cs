9d68deccc00d5adef67ca78cb43bb0942c452a8c
LincThai <78945845+LincThai@users.noreply.github.com>
2025-03-18 10:13:36 +1100

    Add Some Variables to HouseGenerator.cs

--- Assets/Scipts/HouseGenerator.cs
+++ Assets/Scipts/HouseGenerator.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HouseGenerator : MonoBehaviour
 {
+    // numbers for random generation
+    [Header("Randomly Generated Values")]
+    public int numOfFloors;
+    public int numOfRooms;
+
+    // Storage of gameObjects/prefabs to instantiate
+    [Header("List of Generated Objects")]
+    public List<GameObject> floors = new List<GameObject>();
+    public List<GameObject> rooms = new List<GameObject>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

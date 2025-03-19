535157a237b3800d855ee010c832d31d97c3567d
LincThai <78945845+LincThai@users.noreply.github.com>
2025-03-19 15:34:20 +1100

    Spawning Floors and Rooms, Add Scenes

--- Assets/Scipts/HouseGenerator.cs
+++ Assets/Scipts/HouseGenerator.cs
@@ -8,12 +8,15 @@ public class HouseGenerator : MonoBehaviour
     // numbers for random generation
     [Header("Randomly Generated Values")]
     public int numOfFloors;
+    public int floorType;
     public int numOfRooms;
 
     // Storage of gameObjects/prefabs to instantiate
     [Header("List of Generated Objects")]
-    public List<GameObject> floors = new List<GameObject>();
-    public List<GameObject> rooms = new List<GameObject>();
+    public List<GameObject> floorTypes = new List<GameObject>();
+    public List<GameObject> roomTypes = new List<GameObject>();
+
+    List<GameObject> currentHouseComp = new List<GameObject>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,6 +24,7 @@ public class HouseGenerator : MonoBehaviour
         // random number generation
         numOfFloors = Random.Range(1, 4);
         Debug.Log(numOfFloors + " floors");
+        floorType = Random.Range(0, floorTypes.Count);
         // make an array to store the number of room per floor
         // with the length of the arrey equal to the number of floors
         int[] floorRooms = new int[numOfFloors];
@@ -39,10 +43,17 @@ public class HouseGenerator : MonoBehaviour
         
         for (int i = 0; i < floorRooms.Length; i++)
         {
-            // spawn a floor every 3m
-            //Instantiate()
+            // spawn a floor every 3m then add to a list to later be destroyed
+            GameObject floor = Instantiate(floorTypes[floorType], new Vector3(0, i * 3, 0), Quaternion.identity);
+            currentHouseComp.Add(floor);
 
-            for (int j = 0; j < floorRooms[i]; j++) { 
+            for (int j = 0; j < floorRooms[i]; j++)
+            {
+                // spawn room as child of the floor then change transform.localPosition then
+                // add to the list to later be destroyed 
+                GameObject room = Instantiate(roomTypes[Random.Range(0, roomTypes.Count)],
+                    new Vector3(0, 0, 0), Quaternion.identity, floor.transform);
+                currentHouseComp.Add(room);
             }
 
         }

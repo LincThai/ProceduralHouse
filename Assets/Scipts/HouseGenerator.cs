b843776d77dcc16909d021100006ac10368e3010
LincThai <78945845+LincThai@users.noreply.github.com>
2025-03-21 10:13:21 +1100

    More work prepping for models

--- Assets/Scipts/HouseGenerator.cs
+++ Assets/Scipts/HouseGenerator.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class HouseGenerator : MonoBehaviour
 {
+    // editable values
+    [Header("Editable Values")]
     public float floorHeight = 3.0f;
 
     // numbers for random generation
@@ -44,7 +46,8 @@ public class HouseGenerator : MonoBehaviour
         for (int i = 0; i < floorRooms.Length; i++)
         {
             // spawn a floor every 3m then add to a list to later be destroyed
-            GameObject floor = Instantiate(floorTypes[floorType], new Vector3(0, i * 3, 0), Quaternion.identity, transform);
+            GameObject floor = Instantiate(floorTypes[floorType],
+                new Vector3(0, i * floorHeight, 0), Quaternion.identity, transform);
             currentHouseComp.Add(floor);
 
             for (int j = 0; j < floorRooms[i]; j++)

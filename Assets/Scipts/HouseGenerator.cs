ce38bd10efdb303ebc9f3963ee2fdca68c52f1ce
LincThai <78945845+LincThai@users.noreply.github.com>
2025-03-26 15:15:20 +1100

    adjustments

--- Assets/Scipts/HouseGenerator.cs
+++ Assets/Scipts/HouseGenerator.cs
@@ -59,7 +59,7 @@ public class HouseGenerator : MonoBehaviour
             {
                 GameObject stairs = Instantiate(staircase, new Vector3(0, 0, 0), Quaternion.identity, floor.transform);
                 // set position
-                stairs.transform.localPosition = new Vector3(1, 1, 1);
+                stairs.transform.localPosition = new Vector3(1, 1.5f, 1);
             }
 
             for (int j = 0; j < floorRooms[i]; j++)
